0bf2ddaf4a113cb971bae6424cc0779bfd630d29
juanrios456 <103297471+juanrios456@users.noreply.github.com>
2023-10-02 15:10:55 -0500

    Update page.tsx
    Updated a bug for the meta tags

--- app/page.tsx
+++ app/page.tsx
@@ -10,14 +10,14 @@ export const metadata: Metadata = {
   openGraph:{
     title: 'Antonio Masonry and Stucco',
     description: 'Topeka local masonry contractor',
-    url: 'https://www.antoniomasonryandstucco.com/'
+    url: 'https://www.antoniomasonryandstucco.com/',
     siteName: 'Antonio Masonry and Stucco',
     images:[
     {
       url: 'https://www.antoniomasonryandstucco.com/Logo-Final.png',
       width: 640,
       height: 500,
-      alt: 'Logo of Antonios Masonry and Stucco'
+      alt: 'Logo of Antonios Masonry and Stucco',
     },
 }
 
